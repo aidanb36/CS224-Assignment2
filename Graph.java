6482b2a18598e0825594bc91ee1df9613a4147b0
AIdan Brown <aidanb363@gmail.com>
2022-10-01 20:10:12 -0400

    done?

--- Graph.java
+++ Graph.java
@@ -4,6 +4,12 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
+import javax.swing.event.ChangeEvent;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+
 public class Graph {
   ArrayList<Node> nodes;
 
@@ -41,71 +47,81 @@ public class Graph {
     n1.addEdge(n2);
   }
 
-  
+  /**
+   * Need to ensure that the if statement and foreach is correct
+   * @param s
+   * @return
+   */
   public ArrayList<Node> DFS(Node s) {
-    ArrayList<Node> returnL = new ArrayList<>();
-    boolean[] explored = new  boolean[this.nodes.size() + 1];
+    boolean[] visited = new  boolean[this.nodes.size() + 1];
     Stack<Node> stack = new Stack<Node>();
     stack.push(s); 
+    System.out.print(s);
 
     // While the stack is not empty, pop a node from the stack
     while (!stack.isEmpty()) {
       Node u = stack.pop();
-      if(!explored[u.name]){ // if explored[u] = false 
-          explored[u.name] = true;
+      if(!visited[u.name]){ // if explored[u] = false then
+          visited[u.name] = true;
       }
+  
       for (Node v : u.adjlist) {
-        returnL.add(v);
+        stack.add(v);
       }
     }
-    return returnL;
-  } // DFS() 
+    System.out.print(nodes);
+    return nodes;
 
+  } // DFS()
 
   public void findConnectedComponents() {
+
+    //declaring lists
     ArrayList<Node> firstNode;
+    ArrayList<Node> changeList = new ArrayList<>();  //alternating list
     ArrayList<Node> aNode;
     ArrayList<Node> bNode;
-    ArrayList<Node> changeList = new ArrayList<>();
+
+    //returnlist
     ArrayList<Node> returnList = new ArrayList<>();
 
+    //look through nodes
     for(Node a : this.nodes){
-      firstNode = DFS(a);
-      changeList.add(a);
+      firstNode = DFS(a);      //put into first node list
+      changeList.add(a);       //add to the alternting list
       System.out.print("DFS("+a.name+")\n");
 
-      for(Node b : this.nodes){
-        if(firstNode.contains(b)){
-          aNode = DFS(b);
-          
-          for(int i = 0; i < aNode.size();++i){
-            if(!changeList.contains(aNode)){
+      for(Node b : this.nodes){      //loop through nodes with dif list (edge)
+        if(firstNode.contains(b)){   //if node is in first list, add to a list
+          aNode = DFS(b);            //call dfs on node, put into list a
+          for(int i = 0; i < aNode.size();i++){   
+            if(!changeList.contains(aNode)){   //if change doesnt contain nodes a, add to it and dfs at index -> add set to b list
               changeList.add(aNode.get(i));
               bNode = DFS(aNode.get(i));
-
-              for(int q = 0; q < bNode.size(); ++q){
+              for(int q = 0; q<bNode.size(); q++){  //index through b list, add to change
                 changeList.add(bNode.get(q));
               }
             }
           }
         }
       }
-      for(Node e: changeList){
+      for(Node e: changeList){    //loop through change list to get return vals
         if(!returnList.contains(e)){
-          returnList.add(e);
+          returnList.add(e);      //add to return list if contains node e
        }
       }
-      for(int i = 0; i < returnList.size(); ++i){
-        
-        for(int l = i; l<returnList.size(); ++l){
-          if(returnList.get(i).name > returnList.get(l).name){
+      
+      //return list loop
+      for(int i = 0; i<returnList.size(); i++){                //creating 2 index points
+        for(int l = i; l<returnList.size(); l++){   
+          if(returnList.get(i).name > returnList.get(l).name){ //if i>l, set points
             returnList.set(i,returnList.get(i));
             returnList.set(l, returnList.get(l));
           }
         }
       }
-      System.out.print(returnList + "\n");
-      changeList.removeAll(changeList);
+      System.out.print(returnList + "\n");  //print return list
+      changeList.removeAll(changeList);     //clear other lists
       returnList.removeAll(returnList);
     }
   }
